d32b4132bce49492c4fe1eb8ef2f14215bc0d8fe
kalyani-217 <dkalyani2905@gmail.com>
2023-03-29 00:33:19 +0530

    committing one and three files

---
+++ one.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("enter the values of a and b:\n");
+	scanf("%d%d",&a,&b);
+	c=a+b;
+	printf("sum of a and b is=%d\n",c);
+}
---
+++ three.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	int a,n;
+	printf("enter the number to find its cube:\n");
+	scanf("%d",&n);
+	a=n*n*n;
+	printf("the cube value is=%d\n",a);
+}
+
